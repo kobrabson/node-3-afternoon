6d5586339a827d82505c7af01b182a9f6b7c2839
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-08-05 15:00:41 -0600

    done yay

---
+++ index.js
@@ -0,0 +1,35 @@
+
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const ctrl = require('./products_controller')
+
+const app = express();
+
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+})
+.then(dbInstance => {
+    app.set('db', dbInstance)
+})
+.catch(err => {
+    console.log(err)
+});
+
+app.use(express.json())
+
+
+app.post('/api/products', ctrl.create);
+app.get('/api/products', ctrl.getAll);
+app.get('/api/products/:id', ctrl.getOne);
+app.put('/api/products/:id', ctrl.update);
+app.delete('/api/products/:id', ctrl.delete);
+
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`here's your port ${SERVER_PORT}`)
+})
---
+++ my_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE product (
+product_is SERIAL PRIMARY KEY,
+name VARCHAR(40),
+description VARCHAR(80),
+price INT,
+image_url TEXT
+);
